a93d1db6a32bb64e1ac0047894b4630cadf8ea34
Theo Booth <theorlbooth@googlemail.com>
2020-03-05 15:56:07 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -8,13 +8,13 @@ document.body.appendChild(element)
 element.style.textAlign = 'center'
 
 var ul = document.createElement('ul');
- 
+
 for (let i = 0; i < 3; i++) {
   let li = document.createElement('li');
   li.innerHTML = (i + 1).toString();
   ul.appendChild(li);
 }
- 
+
 element.appendChild(ul)
 
 ul.style.textAlign = 'left'
